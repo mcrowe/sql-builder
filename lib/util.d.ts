20fe561c629a0b98012383b07ed9b22ff2cd04a3
Mitch Crowe <crowe.mitch@gmail.com>
2017-04-30 21:34:05 -0700

    Release v0.0.2

---
+++ lib/util.d.ts
@@ -0,0 +1,11 @@
+declare var _default: {
+    isString: (x: any) => boolean;
+    isNumber: (x: any) => boolean;
+    isBoolean: (x: any) => boolean;
+    isNull: (x: any) => boolean;
+    isUndefined: (x: any) => boolean;
+    isDate: (x: any) => boolean;
+    values: (o: any) => any[];
+    mapObject: (o: any, fn: any) => any[];
+};
+export default _default;
